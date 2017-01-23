79f0b089908efb788af5c80654814e3c83d9b59c
Matthew <Bean6754@users.noreply.github.com>
2017-01-23 11:48:05 +0000

    Update main.c

--- Assignment 4/main.c
+++ Assignment 4/main.c
@@ -7,30 +7,41 @@
 // Headers.
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 // Code.
 int main(void) {
-  float l;
-  float f;
-  FILE *filelux;
-  FILE *filefootcandle;
-  
-  filelux = fopen("lux.txt", "w");
-  filefootcandle = fopen("footcandle.txt", "w");
+    float l, f;
+    FILE *filelux;
+    FILE *filefootcandle;
+    
+    filelux = fopen("lux.txt", "w");
+    filefootcandle = fopen("footcandle.txt", "w");
   
   printf("Please enter lux: ");
-  scanf("%f", &l);
+  if (scanf("%f", &l) == 1) {
+    printf("No number inputed.\n");
+  }
+  if (isalpha(l)) {
+    printf("Please enter number.\n");
+  } else {
+    
+  }
   
-  printf("Please enter footcandle: ");
-  scanf("%f", &f);
+  //printf("Please enter footcandle: ");
+  //scanf("%f", &f);
   
   printf("Lux entered: %f\n", l);
   fprintf(filelux, "%f", &l);
   fclose(filelux);
   
-  printf("Footcandle entered: %f\n", f);
-  fprintf(filefootcandle, "%f", &f);
-  fclose(filefootcandle);
+  float footcandle;
+  footcandle = l*2;
+  printf("Footcandle = %f\n", footcandle);
+  
+  //printf("Footcandle entered: %f\n", f);
+  //fprintf(filefootcandle, "%f", &f);
+  //fclose(filefootcandle);
   
   return 0;
 }
