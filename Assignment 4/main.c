d2069c82b94b5d1bd20b2613f942080b12e1300c
Matthew <Bean6754@users.noreply.github.com>
2017-01-23 10:34:59 +0000

    Update main.c

--- Assignment 4/main.c
+++ Assignment 4/main.c
@@ -1,20 +1,36 @@
+/* 
+  File name: main.c
+  Creator: Matthew Dean.
+  Date: 23/01/2017.
+*/ 
+
+// Headers.
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int lux;
-    int footcandle;
-    int x = 0.0929;
-    int y = 10.764;
-    
-    printf("Enter lux value: ");
-    scanf("%d", &lux);
-    printf("Footcandle = %d\n", lux/10.764);
-    
-    printf("Enter footcandle value: ");
-    scanf("%e", &footcandle);
-    printf("Lux = %e\n", footcandle/0.0929);
-    
-    return 0;
+// Code.
+int main(void) {
+  float l;
+  float f;
+  FILE *filelux;
+  FILE *filefootcandle;
+  
+  filelux = fopen("lux.txt", "w");
+  filefootcandle = fopen("footcandle.txt", "w");
+  
+  printf("Please enter lux: ");
+  scanf("%f", &l);
+  
+  printf("Please enter footcandle: ");
+  scanf("%f", &f);
+  
+  printf("Lux entered: %f\n", l);
+  fprintf(filelux, "%f", &l);
+  fclose(filelux);
+  
+  printf("Footcandle entered: %f\n", f);
+  fprintf(filefootcandle, "%f", &f);
+  fclose(filefootcandle);
+  
+  return 0;
 }
-
